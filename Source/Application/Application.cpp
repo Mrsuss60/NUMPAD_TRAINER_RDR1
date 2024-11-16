20aa8598baafe047546c6f236e68ad41b69c4ee3
Mrsuss60 <135135776+Mrsuss60@users.noreply.github.com>
2024-11-16 01:04:48 +0100

    Update Application.cpp

--- Source/Application/Application.cpp
+++ Source/Application/Application.cpp
@@ -11,10 +11,8 @@ void Application::Initialize(HMODULE _Module)
     FILE* stream;
     freopen_s(&stream, "CONOUT$", "w", stdout);
     std::cout << "*************************\n";
-    std::cout << "The Script is Up\n"
-        << "Numpad 1 = Invincibility\n"
-        << "Numpad 2 = Drunk\n"
-        << "Numpad 3 = infinte DeadEyE\n";
+    std::cout << "The Script is Up\n";
+
 
     InputsManager::Register(); 
 
@@ -26,24 +24,24 @@ void Application::Initialize(HMODULE _Module)
             bool Drunk = false;
             bool DeadEye = false;
             bool timeacce = false;
-            int PlayerID = 0;
+
 
             while (true)
             {
+                // controls menu
                 if (Input::IsKeyJustPressed(KEY_F4))
                 {
-                    _PRINT_HELP("PRESS F4 FOR CONTROLS\n NUMPAD 1 = INVINCIBLE\n NUMPAD 2 = DRUNK\n NUMPAD 3 = INFINITE DEADEYE\n", 10.0f, true, 1, 0, 0, 0, 0);
-                }
+                    _PRINT_HELP("PRESS F4 FOR CONTROLS\n NUMPAD 1 = INVINCIBLE\n NUMPAD 2 = DRUNK\n NUMPAD 3 = INFINITE DEADEYE\n  NUMPAD 5 = TIME ACCELERATION\n NUMPAD 6 = DRUNK LASSO\n NUMPAD 9 = KILL LASSO\n", 10.0f, true, 1, 80, 0, 0, 0);
+                }   
 
 
 
-
-
-
+                int PlayerID = 0;
                 Actor playerActor = GET_PLAYER_ACTOR(PlayerID);
                 int actor = GET_LASSO_TARGET(playerActor);
                 std::cout << "\r" << "actor id: " << actor << "     " << std::flush;
 
+                
 
 
 
@@ -65,11 +63,10 @@ void Application::Initialize(HMODULE _Module)
                         Invincible = !Invincible;
                         SET_ACTOR_INVULNERABILITY(playerActor, Invincible);
                         if (Invincible) {
-                            _PRINT_SUBTITLE("<green>Invincible: On!", 1.0f, true, 0, 0, 0, 0, 0);
+                            _PRINT_SUBTITLE("<green>Invincible: On!", 0.5f, true, 0, 0, 0, 0, 0);
                         }
-                        else {
-
-                            _PRINT_SUBTITLE("<red>Invincible: Off!", 1.0f, true, 0, 0, 0, 0, 0);
+                        else { 
+                            _PRINT_SUBTITLE("<red>Invincible: Off!", 0.5f, true, 0, 0, 0, 0, 0);
                         }
                     }
                 }
@@ -78,22 +75,40 @@ void Application::Initialize(HMODULE _Module)
 
 
 
-                //drunk
-                if (Input::IsKeyJustPressed(KEY_NUMPAD_2))
+                //drunk lasso
+                if (Input::IsKeyJustPressed(KEY_NUMPAD_6))
                 {
-                    //if (IS_ACTOR_PLAYER(actor))
-                    //{
                         Drunk = !Drunk;
                         SET_ACTOR_DRUNK(actor, Drunk);
                         if (Drunk)
                         {
-                            _PRINT_SUBTITLE("<green>Drunk:<white> On!", 1.0f, true, 0, 0, 0, 0, 0);
+                            _PRINT_SUBTITLE("<green>Drunk: On!", 0.5f, true, 0, 0, 0, 0, 0);
                         }
-                        else
+                        else if (IS_ACTOR_DRUNK(actor))
                         {
-                            _PRINT_SUBTITLE("<green>Drunk:<white> On!", 1.0f, true, 0, 0, 0, 0, 0);
+                            SET_ACTOR_DRUNK(actor, false);
+                            _PRINT_SUBTITLE("<red>Drunk: Off!", 0.5f, true, 0, 0, 0, 0, 0);
                         }
-                    //}
+                }
+
+
+                //drunk player
+                if (Input::IsKeyJustPressed(KEY_NUMPAD_2))
+                {
+                   if (IS_ACTOR_PLAYER(playerActor))
+                    {
+                    Drunk = !Drunk;
+                    SET_ACTOR_DRUNK(playerActor, Drunk);
+                    if (Drunk)
+                    {
+                        _PRINT_SUBTITLE("<green>Drunk: On!", 0.5f, true, 0, 0, 0, 0, 0);
+                    }
+                    else if(IS_ACTOR_DRUNK(playerActor))
+                    {   
+                        SET_ACTOR_DRUNK(playerActor, false);
+                        _PRINT_SUBTITLE("<red>Drunk: Off!", 0.5f, true, 0, 0, 0, 0, 0);
+                    }
+                    }
                 }
 
 
@@ -109,32 +124,40 @@ void Application::Initialize(HMODULE _Module)
                     //SET_DEADEYE_TIMESCALE(playerActor, 0.5f);
                     if (DeadEye)
                     {
-                        _PRINT_SUBTITLE("<green> Infinite DeadEye : On", 1.0f, true, 0, 0, 0, 0, 0);
+                        _PRINT_SUBTITLE("<green> Infinite DeadEye : On", 0.5f, true, 0, 0, 0, 0, 0);
                     }
                     else
                     {
-                        _PRINT_SUBTITLE("<red> Infinite DeadEye : Off", 1.0f, true, 0, 0, 0, 0, 0);
+                        _PRINT_SUBTITLE("<red> Infinite DeadEye : Off", 0.5f, true, 0, 0, 0, 0, 0);
                     }
                 }
+                if (DeadEye)
+                {
+                    SET_INFINITE_DEADEYE(playerActor, DeadEye);
+                }
+                //
 
+
+                //time acceleration
                 if (Input::IsKeyJustPressed(KEY_NUMPAD_5))
                 {
                     timeacce = !timeacce;
                     if (timeacce)
                     {
                         SET_TIME_ACCELERATION(5000.0f);
-                        _PRINT_SUBTITLE("Time Acceleration: On", 1.0f, true, 0, 0, 0, 0, 0);
+                        _PRINT_SUBTITLE("Time Acceleration: On", 0.5f, true, 0, 0, 0, 0, 0);
                     }
                     else
                     {
-                         SET_TIME_ACCELERATION(30.0f);
-                        _PRINT_SUBTITLE("Time Acceleration: Off", 1.0f, true, 0, 0, 0, 0, 0);
+                        SET_TIME_ACCELERATION(30.0f);
+                        _PRINT_SUBTITLE("Time Acceleration: Off", 0.5f, true, 0, 0, 0, 0, 0);
                     }
                 }
                 if (timeacce)
                 {
                     SET_TIME_ACCELERATION(5000.0f);
                 }
+                //
 
                 WAIT(0);
 
@@ -142,7 +165,6 @@ void Application::Initialize(HMODULE _Module)
             }
         });
 }
-
 
 
 void Application::Shutdown(HMODULE _Module)
