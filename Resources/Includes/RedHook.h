4d3efde1a20c951fa52c1069d74a748e905e2a0f
Mrsuss60 <135135776+Mrsuss60@users.noreply.github.com>
2024-11-17 01:42:00 +0100

    updated

--- Resources/Includes/RedHook.h
+++ Resources/Includes/RedHook.h
@@ -10,9 +10,9 @@
 
 enum LogType
 {
-    Log_Info = 1,
-    Log_Warning,
-    Log_Error
+	Log_Info = 1,
+	Log_Warning,
+	Log_Error
 };
 
 using f_KeyboardHandler = void(*)(DWORD _Key, WORD _Repeats, BYTE _ScanCode, BOOL _IsExtended, BOOL _IsWithAlt, BOOL _WasDownBefore, BOOL _IsUpNow);
@@ -36,14 +36,18 @@ DLL_IMPORT void Print(LogType _Type, const char* _Format, ...);
 
 enum TextAlignment
 {
-    TextAlignment_Left,
-    TextAlignment_Center,
-    TextAlignment_Right
+	TextAlignment_Left,
+	TextAlignment_Center,
+	TextAlignment_Right
 };
 
+DLL_IMPORT uint64_t RH_LoadImage(const char* _FilePath);
+DLL_IMPORT void RH_DrawImage(uint64_t _TextureId, float _X, float _Y, float _Width, float _Height, int _R, int _G, int _B, int _A);
 DLL_IMPORT void RH_DrawRect(float _X, float _Y, float _Width, float _Height, int _R, int _G, int _B, int _A);
 DLL_IMPORT void RH_DrawText(const char* _Text, float _X, float _Y, int _FontId, float _FontScale, int _R, int _G, int _B, int _A, TextAlignment _TextAlignment);
 DLL_IMPORT float RH_GetTextWidth(const char* _Text, int _FontId, float _FontScale);
+DLL_IMPORT void RH_GetTextHeight(const char* _Text, int _FontId, float _FontScale, void(*_Callback)(float));
+DLL_IMPORT void RH_SetTextWrapWidth(float _WrapWidth);
 
 
 
